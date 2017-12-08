14c3a684fdbbbd9db68839cae08563f7367d8afc
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-12-08 14:29:02 -0700

    added file structure

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'pg', debug: false,
+    connection: `postgres://localhost:5432/boomtown_workshop_scheduler_dev`
+    // migrations: {
+    //   directory: path.join(__dirname, 'db', 'migrations')
+    // },
+    // seeds: {
+    //   directory: path.join(__dirname, 'db', 'seeds')
+    // }
+  },
+  test: {
+    client: 'pg', debug: false,
+    connection: `postgres://localhost:5432/boomtown_workshop_scheduler_test`
+    // migrations: {
+    //   directory: path.join(__dirname, 'db', 'migrations')
+    // },
+    // seeds: {
+    //   directory: path.join(__dirname, 'db', 'seeds')
+    // }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
