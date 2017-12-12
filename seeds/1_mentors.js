a66178cff7306f26b76a6b6a1c4ebf4f90c27cc8
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-12-11 20:10:51 -0700

    Integrating

--- seeds/1_mentors.js
+++ seeds/1_mentors.js
@@ -6,8 +6,15 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('mentors').insert([
         { mentor_id: 1, first_name: 'Teddi', last_name: 'Maull' },
-        { mentor_id: 2, first_name: 'Sean', last_name: 'Halvey' },
-        { mentor_id: 3, first_name: 'Jodie', last_name: 'Rigali' }
+        { mentor_id: 2, first_name: 'Sean', last_name: 'Helvey' },
+        { mentor_id: 3, first_name: 'Jodie', last_name: 'Rigali' },
+        { mentor_id: 4, first_name: 'Mark', last_name: 'Oswald' },
+        { mentor_id: 5, first_name: 'Kevin', last_name: 'Cosner' },
+        { mentor_id: 6, first_name: 'Bob', last_name: 'Barker' },
+        { mentor_id: 7, first_name: 'Lucy', last_name: 'Lu' },
+        { mentor_id: 8, first_name: 'Alice', last_name: 'Cooper' },
+        { mentor_id: 9, first_name: 'Marcia', last_name: 'Gray' },
+        { mentor_id: 10, first_name: 'John', last_name: 'Paul' }
       ])
     })
     .then(function() {
