ba978645f072f5b53f883e4654c291e5c29961cd
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-12-13 13:54:19 -0700

    removed extraneous code that was leftover after massive rebase

--- public/js/script.js
+++ public/js/script.js
@@ -159,45 +159,5 @@ var WORKSHOP_ID = null
     window.location.reload(true);
   })
 
-
-
-
-
-
-$('.datepicker').pickadate({
-  selectMonths: true, // Creates a dropdown to control month
-  selectYears: 15, // Creates a dropdown of 15 years to control year,
-  today: 'Today',
-  clear: 'Clear',
-  close: 'Ok',
-  closeOnSelect: false // Close upon selecting a date,
-})
-
-$('.timepicker').pickatime({
-  default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-  fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
-  twelvehour: true, // Use AM/PM or 24-hour format
-  donetext: 'OK', // text for done-button
-  cleartext: 'Clear', // text for clear-button
-  canceltext: 'Cancel', // Text for cancel-button
-  autoclose: false, // automatic close timepicker
-  ampmclickable: true, // make AM PM clickable
-  vibrate: true, // vibrate the device when dragging clock hand
-  aftershow: function() {} //Function for after opening timepicker
-
-  const receivedInfo = {
-    name: workshopName,
-    mentors: selectedMentorsArray,
-    date: workshopDate,
-    start_time: workshopStartTime,
-    end_time: workshopEndTime
-  }
-  axios.post('/workshops', {receivedInfo})
-  .then(function (response){
-    console.log(response);
-  })
-})
-
-
 //end of document ready
 })
