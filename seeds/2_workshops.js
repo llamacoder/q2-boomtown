10df16c97206b70dc06bd9ce91e95f1fc928a83b
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-12-14 01:50:54 -0700

    working data visualization

--- seeds/2_workshops.js
+++ seeds/2_workshops.js
@@ -22,8 +22,57 @@ exports.seed = function(knex, Promise) {
           },
           {
             workshop_id: 3,
-            name: 'Financial Analysis',
+            name: 'Running a SAAS Business',
             date: '12/19/17',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 4,
+            name: 'Green Company Logistics',
+            date: '12/20/17',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 5,
+            name: 'Branding',
+            date: '1/17/18',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 6,
+            name: 'Websites for New Businesses',
+            date: '1/7/18',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 7,
+            name: 'IT for New Businesses',
+            date: '11/17/17',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 8,
+            name: 'Production Cycles',
+            date: '11/24/17',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 9,
+            name: 'How to Write a Business Plan',
+            date: '1/5/18',
+            start_time: '01:00PM',
+            end_time: '03:00PM'
+          },
+          {
+            workshop_id: 10,
+            name: 'Financial Analysis',
+            date: '12/8/17',
             start_time: '10:00AM',
             end_time: '11:00AM'
           }
--- seeds/3_mentors_workshops.js
+++ seeds/3_mentors_workshops.js
@@ -6,8 +6,17 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('mentors_workshops').insert([
         { mentor_id: 1, workshop_id: 1 },
-        { mentor_id: 2, workshop_id: 1 },
-        { mentor_id: 3, workshop_id: 2 },
+        { mentor_id: 5, workshop_id: 1 },
+        { mentor_id: 2, workshop_id: 2 },
+        { mentor_id: 8, workshop_id: 3 },
+        { mentor_id: 8, workshop_id: 4 },
+        { mentor_id: 8, workshop_id: 5 },
+        { mentor_id: 3, workshop_id: 6 },
+        { mentor_id: 6, workshop_id: 7 },
+        { mentor_id: 4, workshop_id: 8 },
+        { mentor_id: 4, workshop_id: 9 },
+        { mentor_id: 3, workshop_id: 10 },
+        { mentor_id: 1, workshop_id: 10 },
       ])
     })
 }
